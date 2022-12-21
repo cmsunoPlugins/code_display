f8d4a0841e623d71f49db68ac253fcff1f285108
Jacques Malgrange <jacques.malgrange@gmail.com>
2022-12-21 17:24:37 +0100

    V1.3

--- code_displayMake.php
+++ code_displayMake.php
@@ -5,7 +5,7 @@ if(!isset($_SESSION['cmsuno'])) exit();
 $q1 = '';
 if(file_exists('data/'.$Ubusy.'/codeDisplay.json')) $q1 = file_get_contents('data/'.$Ubusy.'/codeDisplay.json');
 else if(file_exists('data/codeDisplay.json')) $q1 = file_get_contents('data/codeDisplay.json');
-if($q1 && strpos($Ucontent,'</code>')!==false) {
+if($q1 && strpos($Ucontent,'/pre')!==false) {
 	$a1 = json_decode($q1,true);
 	if(isset($a1['css']) && file_exists('plugins/code_display/highlight/styles/'.$a1['css'])) $Uhead .= '<link rel="stylesheet" href="uno/plugins/code_display/highlight/styles/'.$a1['css'].'">'."\r\n";
 	else $Uhead .= '<link rel="stylesheet" href="uno/plugins/code_display/highlight/styles/androidstudio.min.css">'."\r\n";
